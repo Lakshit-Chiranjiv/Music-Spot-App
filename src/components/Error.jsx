7711c3abfd5cf7eb7f3dc65fa8bb8fa2f92d5e0d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-27 13:00:48 +0530

    error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className='w-full flex justify-center items-center'>
+    <h1 className='text-2xl text-red-400 font-bold mt-2'>Something went wrong. Try again.</h1>
+  </div>
 );
 
 export default Error;
