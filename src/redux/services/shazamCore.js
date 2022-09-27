d5dd7c2fd82f59c471defc7a1159cc4467059af1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-27 12:41:41 +0530

    setup for the api endpoint structure

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key','8993388464mshf5065643c712524p15a48fjsn75dcf75bfe68')
+
+            return headers;
+        }
+    }),
+
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: 'charts/world' }),
+    })
+})
+
+export const { useGetTopChartsQuery } = shazamCoreApi
