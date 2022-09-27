44485c047adb3185d18f6691c1de04c9a6ae7581
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-27 12:58:42 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,10 @@
-const Loader = () => (
-  <div>Loader</div>
+import { loader } from './../assets'
+
+const Loader = ({text}) => (
+  <div className='flex flex-col w-full justify-center items-center'>
+    <img src={loader} alt="loader" className='w-32 h-32 object-contain'/>
+    <h1 className='text-2xl text-white mt-2 font-bold'>{text || 'Loading...'}</h1>
+  </div>
 );
 
 export default Loader;
