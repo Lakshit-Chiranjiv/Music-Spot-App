0500023048678387181f028cd7011bfa8cc9bbcb
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-03 23:07:53 +0530

    made required imports for top plays

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,20 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { FreeMode } from "swiper";
+import { Swiper,SwiperSlide } from "swiper/react";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+
+const TopPlay = () => {
+
+  return (
+    <div></div>
+  )
+};
 
 export default TopPlay;
