1d1b9c973b8ef92b8fbe7e09b0b1fe3deddaf9b6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-03 19:49:47 +0530

    called navlinks and rendered

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+
 import { RiCloseLine } from 'react-icons/ri'
 import { logo } from './../assets/index.js'
-import { links } from './../assets/constants.js' 
+import NavLinks from "./NavLinks.jsx";
+
 
 const Sidebar = () => {
 
@@ -11,6 +12,7 @@ const Sidebar = () => {
   return (
     <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#342e4a]">
       <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
+      <NavLinks/>
     </div>
   )
 };
