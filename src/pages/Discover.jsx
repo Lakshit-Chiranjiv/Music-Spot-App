9eb8ae018d61a8b4b7bd0befb2295ed83af19d7b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-03 15:57:04 +0530

    imported usedispatch and useselector and got active song nd isplaying state

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,13 @@
 import { Error, SongCard, Loader } from "../components";
 import { genres } from './../assets/constants.js'
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useDispatch, useSelector } from "react-redux";
 
 const Discover = () => {
+
+    const dispatch = useDispatch()
+
+    const { activeSong, isPlaying } = useSelector((state) => state.player)
 
     const { data, isFetching, error } = useGetTopChartsQuery()
 
