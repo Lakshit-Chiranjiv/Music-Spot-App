da9f4282a59d6f2bdb00cb5fc68420633d2bbdf9
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-03 19:35:24 +0530

    logo and sidebar styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,15 @@ import { RiCloseLine } from 'react-icons/ri'
 import { logo } from './../assets/index.js'
 import { links } from './../assets/constants.js' 
 
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+const Sidebar = () => {
+
+  const [mobileMenuOpen,setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#342e4a]">
+      <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
+    </div>
+  )
+};
 
 export default Sidebar;
