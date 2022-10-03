9b860f37ae7459a9e698ecdc7242265541e49d4c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-03 19:57:27 +0530

    conditional icon render for mobile size

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import { HiOutlineMenu } from 'react-icons/hi'
 import { RiCloseLine } from 'react-icons/ri'
 import { logo } from './../assets/index.js'
 import NavLinks from "./NavLinks.jsx";
@@ -10,10 +11,20 @@ const Sidebar = () => {
   const [mobileMenuOpen,setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#342e4a]">
-      <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
-      <NavLinks/>
-    </div>
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#342e4a]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
+        <NavLinks/>
+      </div>
+
+      <div className="md:hidden block absolute top-6 right-4">
+        {
+          mobileMenuOpen ? 
+          <RiCloseLine className="w-6 h-6 mr-2 text-white"/> :
+          <HiOutlineMenu className="w-6 h-6 mr-2 text-white"/>
+        }
+      </div>
+    </>
   )
 };
 
