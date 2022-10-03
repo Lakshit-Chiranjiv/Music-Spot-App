93618f164e02ec1708d2811056270dbe8c4aadae
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-03 16:39:30 +0530

    component conditional rendering and layout

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,17 @@
-const PlayPause = () => (
-  <div>Loader</div>
+import { FaPlayCircle, FaPauseCircle } from 'react-icons/fa'
+
+const PlayPause = ({isPlaying, activeSong, song, handlePause, handlePlay}) => (
+  isPlaying && activeSong?.title===song.title ? 
+  (<FaPauseCircle
+    className='text-gray-300'
+    size={36}
+    onClick={handlePause}
+  />) :
+  (<FaPlayCircle
+    className='text-gray-300'
+    size={36}
+    onClick={handlePlay}
+  />)
 );
 
 export default PlayPause;
