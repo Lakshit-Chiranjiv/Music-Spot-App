e7fb0e7f59b426f21cc2d25bc517f8eb2d84c8d4
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-08 17:11:31 +0530

    top chart card img and title

--- src/components/TopChartCard.jsx
+++ src/components/TopChartCard.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
 
 const TopChartCard = ({song,i}) => {
   return (
     <div className="flex w-full items-center rounded-lg cursor-pointer mb-2 py-2 p-4 hover:bg-[#4c426e]">
-        {song.title}
+        <h3 className="font-bold text-white text-base mr-3">{i+1}.</h3>
+        <div className="flex flex-1 justify-between items-center">
+          <img src={song?.images.coverart} alt="song cover img" className="w-20 h-20 rounded-lg"/>
+          <div className="flex-1 flex flex-col justify-between mx-3">
+            <Link to={`/songs/${song?.key}`} className='font-bold text-white text-xl'>{song?.title}</Link>
+            <Link to={`/artists/${song?.artists[0].adamid}`} className='text-gray-400 text-base mt-2'>{song?.subtitle}</Link>
+          </div>
+        </div>
     </div>
   )
 }
