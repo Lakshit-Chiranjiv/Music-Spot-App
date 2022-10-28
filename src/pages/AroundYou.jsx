92207646c65f768edd6084dd18d5655978adba2c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-29 00:10:58 +0530

    useEffect and fetchCountry call

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -9,6 +9,23 @@ const AroundYou = () => {
     const [loading, setLoading] = useState(true)
 
     const { isPlaying, activeSong } = useSelector((state) => state.player)
+
+    useEffect(() => {
+        const fetchCountry = async() => {
+            try {
+                const apiData = await axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_jxBO5JK9yuR4mUDeVEjR5sYeTESF7') 
+                setCountry(apiData?.data?.location?.country)
+            } catch (error) {
+                console.log(error)
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+
+        fetchCountry()
+
+    }, [country])
     
     return (
         <div>
