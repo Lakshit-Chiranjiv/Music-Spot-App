d4d2a17e7218ed178b775fd2d10876b25d07d26e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-29 18:08:42 +0530

    Around you structure and render

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -37,8 +37,26 @@ const AroundYou = () => {
         return <Error/>
     
     return (
-        <div>
+        <div className='flex flex-col'>
+            <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>
+                Around You &nbsp;
+                <span className='text-base font-black'>{country}</span>
+            </h2>
 
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {
+                    data?.map((song,i) => (
+                        <SongCard
+                            key={song.key}
+                            i={i}
+                            song={song}
+                            isPlaying={isPlaying}
+                            activeSong={activeSong}
+                            data={data}
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 };
