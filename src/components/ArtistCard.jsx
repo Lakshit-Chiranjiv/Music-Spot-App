aa22638cbf4e03257f61569cdd336de1ed3ccdd5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-29 20:12:32 +0530

    navigate url corrected

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -5,7 +5,7 @@ const ArtistCard = ({track}) => {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer" onClick={() => navigate(`artists/${track?.artists[0].adamid}`)}>
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer" onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}>
       <img src={track?.images?.coverart} alt="artist" className="h-56 w-full rounded-lg"/>
       <p className="mt-4 text-white font-semibold truncate text-lg">{track?.subtitle}</p>
     </div>
