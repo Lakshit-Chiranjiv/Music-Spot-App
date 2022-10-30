53247b31152a2b026f1f2ed55212cf123900fa28
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-30 20:19:22 +0530

    corrected overlay render on songcard condition

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -20,7 +20,7 @@ const SongCard = ({song, i, isPlaying, activeSong, data}) => {
   return (
   <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
     <div className="relative w-full h-56 group">
-      <div className={`absolute inset-0 items-center justify-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+      <div className={`absolute inset-0 items-center justify-center bg-black bg-opacity-50 group-hover:flex ${(activeSong?.key === song.key && activeSong?.title === song.title) ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
         <PlayPause
           song={song}
           handlePause={handlePauseClick}
