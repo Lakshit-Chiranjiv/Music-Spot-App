29a729eab05baaf1c0488278c27a1b37ed17da0e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-30 12:46:20 +0530

    search page done

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,46 @@
-const Search = () => (
-  <div>Search</div>
-);
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsBySearchQuery } from '../redux/services/shazamCore.js';
+
+const Search = () => {
+
+    const { searchSongName } = useParams()
+    const { isPlaying, activeSong } = useSelector((state) => state.player)
+
+    const { data, isFetching, error } = useGetSongsBySearchQuery(searchSongName)
+
+    const songs = data?.tracks?.hits?.map((song) => song.track)
+
+    if(isFetching)
+        return <Loader text='Loading Top Charts'/>
+
+    if(error)
+        return <Error/>
+    
+    return (
+        <div className='flex flex-col'>
+            <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>
+                Showing Results for <span className='font-black'>{searchSongName}</span>
+            </h2>
+
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {
+                    songs?.map((song,i) => (
+                        <SongCard
+                            key={song.key}
+                            i={i}
+                            song={song}
+                            isPlaying={isPlaying}
+                            activeSong={activeSong}
+                            data={data}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
+};
 
 export default Search;
